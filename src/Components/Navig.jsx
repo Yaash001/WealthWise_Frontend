d1a4f7ca5e257daf46ad0915b9ecf8a146d113b2
Yash <heyyaash01@gmail.com>
2025-02-05 23:34:27 +0530

    Tried To Make Some Changes In The Navigation Bar,But Failed To Do So

--- src/Components/Navig.jsx
+++ src/Components/Navig.jsx
@@ -1,17 +1,23 @@
-import {Link} from 'react-dom';
+import {Link} from 'react-router-dom';
 import './Navig.css';
 function Navig() {
     return (
 <nav>
     <h1>WealthWise</h1>
     <div>
-        <a href="/Login">Login</a>
-        <a href="/SignUp">Signup</a>
-        <a href='/About'>About</a>
+       <Link to='/Login'>LOGIN</Link>
+    
+        <Link to='/Signup'>SIGNUP</Link>
+        
+        <Link to='/About'>ABOUT</Link>
+        {/*}
+        <a href='/Signp'>SIGNUP</a>
+        <a href='/Login'>LOGIN</a>
+        <a href='/About'>ABOUT</a>*/}
     </div>
 </nav>
 
     );   
-  }
+  };
   export default Navig;
   
